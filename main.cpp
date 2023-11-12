785cf62e72522966c9b0cbded9bfa74bb813a134
Humberto Aleman <haleman.23@est.ucab.edu.ve>
2023-11-12 17:52:01 -0400

    Standarized Types in main.cpp

--- main.cpp
+++ main.cpp
@@ -23,8 +23,8 @@ struct Vendors {
 };
 
 struct ArticleNode { Article data; int key; ArticleNode* next = NULL; };
-struct ClientNode { Client data; ClientNode* next = NULL; };
-struct VendorsNode { Vendors data; VendorsNode* next = NULL; };
+struct ClientNode { Client data; int key; ClientNode* next = NULL; };
+struct VendorsNode { Vendors data; int key; VendorsNode* next = NULL; };
 
 // OTHER FUNCTIONS
 
@@ -32,98 +32,90 @@ struct VendorsNode { Vendors data; VendorsNode* next = NULL; };
 
 // ARTICLES
 
-int GetMaxKey(ArticleNode* list) {
+template <typename T_ptr>
+int GetMaxKey(T_ptr* list) {
 	if (list == NULL) { return 0; }
 	int max = 0;
 	while (list->next != NULL) {
 		if (list->key > max) { max = list->key; }
 		list = list->next;
-	}
-	if (list->key > max) { max = list->key; }
+	} if (list->key > max) { max = list->key; }
 	return ++max;
 }
 
 // Add at begining
-void PushArticle(ArticleNode* &list, std::string code, int price, int ammount, std::string name) {
-	ArticleNode* tmp = new ArticleNode;
+template <typename T_ptr, typename T>
+void PushElement(T_ptr* &list, T data) {
+	T_ptr* tmp = new T_ptr;
 	tmp->next = list;
-	tmp->key = GetMaxKey(list);
-	tmp->data.code = code;
-	tmp->data.price = price;
-	tmp->data.ammount = ammount;
-	tmp->data.name = name;
+	tmp->key = GetMaxKey<T_ptr>(list);
+	tmp->data = data;
 	list = tmp;
 };
 
 // Add at end
-void AppendArticle(ArticleNode* &list, std::string code, int price, int ammount, std::string name) {
-	if (list == NULL) { PushArticle(list, code, price, ammount, name); return; }
-	ArticleNode* tmp = list;
+template <typename T_ptr, typename T>
+void AppendElement(T_ptr* &list, T data) {
+	if (list == NULL) { PushElement<T_ptr, T>(list, data); return; }
+	T_ptr* tmp = list;
 	while (tmp->next != NULL) {
 		tmp = tmp->next;
-	};
-	tmp->next = new ArticleNode;
-	tmp->next->key = GetMaxKey(list);
-	tmp->next->data.code = code;
-	tmp->next->data.price = price;
-	tmp->next->data.ammount = ammount;
-	tmp->next->data.name = name;
+	}; tmp->next = new T_ptr;
+	tmp->next->key = GetMaxKey<T_ptr>(list);
+	tmp->next->data = data;
 };
 
 // Add at specified position
-void InsertArticle(ArticleNode* &list, std::string code, int price, int ammount, std::string name, int position) {
+template <typename T_ptr, typename T>
+void InsertElement(T_ptr* &list, T data, int position) {
 	if (position < 0) { printf("Cannot insert article in negative position"); return; }
-	if (list == NULL || position == 0) { PushArticle(list, code, price, ammount, name); return; }
-	ArticleNode* tmp = list;
+	if (list == NULL || position == 0) { PushElement<T_ptr, T>(list, data); return; }
+	T_ptr* tmp = list;
 	while (tmp->next != NULL && position != 1) {
 		tmp = tmp->next; position--;
-	}
-	ArticleNode* aux = tmp->next;
-	int key = GetMaxKey(list);
-	tmp->next = new ArticleNode;
-	tmp->next->key = key; 
-	tmp->next->data.code = code;
-	tmp->next->data.price = price;
-	tmp->next->data.ammount = ammount;
-	tmp->next->data.name = name;
+	} T_ptr* aux = tmp->next;
+	int key = GetMaxKey<T_ptr>(list);
+	tmp->next = new T_ptr;
+	tmp->next->key = key;
+	tmp->next->data = data;
 	tmp->next->next = aux;
 	return;
 };
 
 // Remove at position (if no position specified, at end)
-Article PopArticle(ArticleNode* &list, int position = -1) {
-	if (list == NULL) { printf("Cannot read value from NULL list, returning empty article\n"); return *new Article; }
-	if (list->next == NULL) { Article aux = list->data; delete list; list = NULL; return aux; }
-	ArticleNode* tmp = list;
+template <typename T_ptr, typename T>
+T PopArticle(T_ptr* &list, int position = -1) {
+	if (list == NULL) { printf("Cannot read value from NULL list, returning empty article\n"); return *new T; }
+	if (list->next == NULL) { T aux = list->data; delete list; list = NULL; return aux; }
+	T_ptr* tmp = list;
 	while (tmp->next->next != NULL) {
 		if (position == 1) { break; }
 		tmp = tmp->next; position--;
-	} ArticleNode* aux = tmp->next->next;
-	Article ret_val = tmp->next->data;
+	} T_ptr* aux = tmp->next->next;
+	T ret_val = tmp->next->data;
 	delete tmp->next; tmp->next = aux;
 	return ret_val;
 };
 
 // Read at position (if no position specified, at end)
-Article ReadArticle(ArticleNode* list, int position = -1) {
-	if (list == NULL) { printf("Cannot read value from NULL list, returning empty article\n"); return *new Article; }
+template <typename T_ptr, typename T>
+T ReadArticle(T_ptr* list, int position = -1) {
+	if (list == NULL) { printf("Cannot read value from NULL list, returning empty article\n"); return *new T; }
 	if (list->next == NULL) { return list->data; }
 	while (list->next->next != NULL && position != 1) {
 		list = list->next; position--;
 	} return list->next->data;
 };
 
-void ModifyArticle(ArticleNode* list, int position, std::string code, int price, int ammount, std::string name) {
+template <typename T_ptr, typename T>
+void ModifyArticle(T_ptr* list, int position, T data) {
 	while (list->next != NULL && position > 0) {
 		list = list->next; position--;
-	}
-	list->data.code = code;
-	list->data.price = price;
-	list->data.ammount = ammount;
-	list->data.name = name;
+	} list->data = data;
 };
 
-void PrintArticles(ArticleNode* list) {
+template <typename T_ptr>
+void PrintElements(T_ptr* list) {
 	if (list == NULL) { printf("NULL\n"); return; }
 	while (list->next != NULL) {
 		printf("%s -> ", &list->data.name[0]);
@@ -131,7 +123,8 @@ void PrintArticles(ArticleNode* list) {
 	} printf("%s -> NULL\n", &list->data.name[0]);
 };
 
-void PrintKeys(ArticleNode* list) {
+template <typename T_ptr>
+void PrintKeys(T_ptr* list) {
 	if (list == NULL) { printf("NULL\n"); return; }
 	while (list->next != NULL) {
 		printf("%d -> ", list->key);
@@ -139,22 +132,40 @@ void PrintKeys(ArticleNode* list) {
 	} printf("%d -> NULL\n", list->key);
 };
 
+Article CreateArticle(std::string code, int price, int ammount, std::string name) {
+	Article ret;
+	ret.code = code;
+	ret.price = price;
+	ret.ammount = ammount;
+	ret.name = name;
+	return ret;
+};
+
+Client CreateClient(int idNumber, std::string address, std::string cellphoneNumber, std::string name) {
+	Client ret;
+	ret.idNumber = idNumber;
+	ret.address = address;
+	ret.cellphoneNumber = cellphoneNumber;
+	ret.name = name;
+	return ret;
+};
+
 // END ARTICLES
 
 int main() {
-	ArticleNode* InitialNode = NULL;
-	InsertArticle(InitialNode, "CEA", 10, 1, "Pootis Engage CD", 4);
-	InsertArticle(InitialNode, "CEA", 10, 1, "Big Chungus Action Figure", 0);
-	InsertArticle(InitialNode, "CEA", 10, 1, "Skibidi Toilet Collectors Edition", 0);
-	InsertArticle(InitialNode, "CEA", 10, 1, "Fanum Tax Reduction Notice", 0);
-	InsertArticle(InitialNode, "CEA", 10, 1, "Lobotomy Kit", 10);
-	InsertArticle(InitialNode, "CEA", 10, 1, "Lobotomy Kit", 3);
-	InsertArticle(InitialNode, "CEA", 10, 1, "Lobotomy Kit", 3);
+	ClientNode* InitialNode = NULL;
+	PushElement<ClientNode, Client>(InitialNode,
+		CreateClient(30142718, "My Home", "04143103358", "Humberto Aleman"));
+	PushElement<ClientNode, Client>(InitialNode,
+		CreateClient(30395024, "My Home", "04143103358", "Cristina Carnevali"));
+	AppendElement<ClientNode, Client>(InitialNode,
+		CreateClient(30142718, "My Home", "04143103358", "Humberto Aleman"));
+	AppendElement<ClientNode, Client>(InitialNode,
+		CreateClient(30395024, "My Home", "04143103358", "Cristina Carnevali"));
+	InsertElement<ClientNode, Client>(InitialNode,
+		CreateClient(30395024, "My Home", "04143103358", "Doug"), 1);
 
-	PrintArticles(InitialNode);
-	PrintKeys(InitialNode);
-	printf("%d", GetMaxKey(InitialNode));
-
-
+	PrintElements<ClientNode>(InitialNode);
+	PrintKeys<ClientNode>(InitialNode);
 	return 0;
 }
